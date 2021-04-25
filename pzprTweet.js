727b14c834f21a7fb32b17b5aa1050c999bd7455
smynudop <smyn326@gmail.com>
2021-04-25 18:35:56 +0900

    resize image and other

--- pzprTweet.js
+++ pzprTweet.js
@@ -20,6 +20,7 @@ class PzprTweet {
 
         if (true || form.isAttachImg) {
             imgData = await PzprTweet.fetchImg(form.url)
+            imgData = await PzprTweet.extendImg(imgData)
             imgId = await PzprTweet.uploadImg(client, imgData)
         }
         const tweetId = await PzprTweet.update(client, form.tweet_text, imgId)
@@ -27,15 +28,50 @@ class PzprTweet {
     }
 
     static async fetchImg(url) {
-        url = url.replace(/https?:\/\/pzv\.jp\/p.html\?/, "https://puzz.link/pv?frame=3&")
-        url = url.replace(/https?:\/\/pzv\.jp\/p\?/, "https://puzz.link/pv?frame=3&")
+        url = url.replace(/https?:\/\/pzv\.jp\/p.html\?/, "https://puzz.link/pv?")
+        url = url.replace(/https?:\/\/pzv\.jp\/p\?/, "https://puzz.link/pv?")
 
-        url = url.replace(/https?:\/\/puzz.link\/p\?/, "https://puzz.link/pv?frame=3&")
+        url = url.replace(/https?:\/\/puzz.link\/p\?/, "https://puzz.link/pv?")
 
         const res = await fetch(url)
         if (!res.ok) throw new Error("画像が正常に取得できませんでした")
         const blob = await res.arrayBuffer()
         return Buffer.from(blob)
+    }
+
+    static async extendImg(imgBuffer) {
+        const sharp = require("sharp")
+
+        let baseSharp = sharp(imgBuffer)
+        let { width: baseWidth, height: baseHeight } = await baseSharp.metadata()
+
+        const newWidth = Math.ceil(((baseHeight + 20) * 16) / 9)
+
+        if (baseWidth + 20 >= newWidth) {
+            return await baseSharp
+                .extend({
+                    top: 10,
+                    bottom: 10,
+                    left: 10,
+                    right: 10,
+                    background: "#FFFFFF",
+                })
+                .toBuffer()
+        } else {
+            return await baseSharp
+                .extend({
+                    top: 10,
+                    bottom: 10,
+                    left: 10,
+                    right: 10,
+                    background: "#FFFFFF",
+                })
+                .resize(newWidth, baseHeight, {
+                    fit: "contain",
+                    background: "#FFFFFF",
+                })
+                .toBuffer()
+        }
     }
 
     static async uploadImg(client, img) {
